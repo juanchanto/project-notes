90016afdf699c2423edfa509a04e7fe3428ef46e
juanchanto <juanchanto96@gmail.com>
2024-10-29 22:28:37 -0600

    Commit with style Login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,34 @@
-import { login, signup } from './actions'
+import { login, signup } from './actions';
+import styles from './LoginPage.module.css'; // Asegúrate de importar el CSS
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
-  )
+    <div className={styles.container}>
+      <h1 className={styles.title}>Iniciar Sesión</h1>
+      <form className={styles.form}>
+        <label htmlFor="email" className={styles.label}>Email:</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          required
+          className={styles.input}
+        />
+        
+        <label htmlFor="password" className={styles.label}>Contraseña:</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          required
+          className={styles.input}
+        />
+        
+        <div className={styles.buttonContainer}>
+          <button type="submit" formAction={login} className={styles.button}>Iniciar Sesión</button>
+          <button type="submit" formAction={signup} className={`${styles.button} ${styles.signupButton}`}>Registrarse</button>
+        </div>
+      </form>
+    </div>
+  );
 }
