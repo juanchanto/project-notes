ccbbb128e154116df7f8b308a46c98c47cdbd294
juanchanto <juanchanto96@gmail.com>
2024-10-30 22:13:43 -0600

    Commit with css for open notes

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,28 +1,28 @@
 import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
-import styles from '@/app/Notes.module.css'; // Asegúrate de que la ruta sea correcta
+import styles from '@/app/Notes.module.css';
 import { Note } from '@/lib/types';
-import { signout } from '@/app/login/actions';
 import HeaderAuth from '../components/HeaderAuth';
 
 export default async function PrivateNotes() {
-    const supabase = await createClient()
+    const supabase = await createClient();
 
-    const { data, error } = await supabase.auth.getUser()
+    const { data, error } = await supabase.auth.getUser();
     if (error || !data?.user) {
-        redirect('/login')
+        redirect('/login');
     }
+    
     const { data: notes }: { data: Note[] | null } = await supabase.from("notes").select();
 
     return (
         <div className={styles.container}>
             <HeaderAuth/>
             <p>Hello {data.user.email} </p>
-            <button type="button" onClick={signout} className={styles.button}>Cerrar Sesión</button>
             <h1 className={styles.title}>Notas</h1>
+            <button className={styles.backupButton}>Hacer Respaldo</button>
             <div className={styles.notesContainer}>
                 {notes ? (
-                    notes.map(note => ( // Usa 'note' aquí o define una interfaz para las notas
+                    notes.map((note: Note) => (
                         <div key={note.id} className={styles.noteCard}>
                             <h2 className={styles.noteTitle}>{note.title}</h2>
                             <p className={styles.noteContent}>{note.content}</p>
@@ -38,4 +38,3 @@ export default async function PrivateNotes() {
         </div>
     );
 }
-
